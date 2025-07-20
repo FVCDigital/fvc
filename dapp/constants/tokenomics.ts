d29355e5de08647e9f45c396fe777c9d887c263f
Igluminati <IgliKristo@protonmail.com>
2025-07-20 09:50:43 +0100

    delete files and npm lockfile for yarn-only workspace

--- dapp/constants/tokenomics.ts
+++
@@ -1,7 +0,0 @@
-// DO NOT MODIFY WITHOUT APPROVAL
-export const FVCG_SUPPLY = 1_000_000_000;
-export const FVCG_SYMBOL = "FVCG";
-export const FVC_SYMBOL = "FVC";
-export const STAKING_APY_MIN = 8;
-export const STAKING_APY_MAX = 12;
-export const BOND_VEST_MONTHS = 6; 
--- dapp/screens/StubScreen.tsx
+++
@@ -1,8 +0,0 @@
-export default function StubScreen() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      <h1 className="text-2xl font-bold">Stub Screen</h1>
-      <p>This is a placeholder.</p>
-    </div>
-  );
-} 
