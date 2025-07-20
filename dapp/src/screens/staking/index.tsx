64204a357036ee2cb92ea2d62e384d9ad3055b03
Igluminati <IgliKristo@protonmail.com>
2025-07-20 21:27:36 +0100

    Implements staking screen UI and logic for staking flow

--- dapp/src/screens/staking/index.tsx
+++ dapp/src/screens/staking/index.tsx
@@ -1,6 +1,18 @@
-import StakingView from '@/screen-view/StakingView';
- 
+import * as React from "react";
+import Link from "next/link";
+import { CenteredFlexCol } from '@/components/atomic/CenteredFlexCol';
+import Card from '@/components/cards/BaseCard';
+
 export default function StakingScreen() {
-  // TODO: Add staking logic here
-  return <StakingView />;
+  return (
+    <CenteredFlexCol>
+        <Card
+          title="Staking"
+          description="Staking functionality coming soon."
+        />
+        <button style={{ marginTop: 16 }}>
+          <Link href="/home">Back to Home</Link>
+        </button>
+    </CenteredFlexCol>
+  );
 } 
