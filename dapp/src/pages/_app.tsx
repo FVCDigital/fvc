9ef362b7cce609031754a6273146614f3cb834cd
Igluminati <IgliKristo@protonmail.com>
2025-07-20 21:22:00 +0100

    Implements the global entry point file _app.tsx for global app styling and provider setup

--- dapp/src/pages/_app.tsx
+++ dapp/src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import '@/styles/globals.css';
 import '@rainbow-me/rainbowkit/styles.css';
 import type { AppProps } from 'next/app';
@@ -5,6 +6,7 @@ import { getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import { WagmiProvider } from 'wagmi';
 import { mainnet, polygon, arbitrum } from 'wagmi/chains';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { theme } from '@/constants/theme';
 
 const projectId = '54d704a3ccdf59ce20365f11281123bd';
 
@@ -19,13 +21,15 @@ const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}>
-        <RainbowKitProvider>
-          <Component {...pageProps} />
-        </RainbowKitProvider>
-      </QueryClientProvider>
-    </WagmiProvider>
+    <div style={{ background: theme.appBackground, minHeight: '100vh', minWidth: '100vw' }}>
+      <WagmiProvider config={config}>
+        <QueryClientProvider client={queryClient}>
+          <RainbowKitProvider>
+            <Component {...pageProps} />
+          </RainbowKitProvider>
+        </QueryClientProvider>
+      </WagmiProvider>
+    </div>
   );
 }
 
