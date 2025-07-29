f40d2c9f1231bf94434da97553db1d6f79431a96
Igluminati <IgliKristo@protonmail.com>
2025-07-29 21:24:34 +0100

    Fixes FVCOutput export name in TradingCard index

--- dapp/src/components/cards/TradingCard/index.ts
+++ dapp/src/components/cards/TradingCard/index.ts
@@ -3,6 +3,6 @@ export { default as TabSwitcher } from './TabSwitcher';
 export { default as AssetSelector } from './AssetSelector';
 export { default as BondingProgressBar } from './BondingProgressBar';
 export { default as AmountInput } from './AmountInput';
-export { default as FVCGOutput } from './FVCOutput';
+export { default as FVCOutput } from './FVCOutput';
 export { default as CardPaymentForm } from './CardPaymentForm';
 export { default as BondingTerms } from './BondingTerms'; 
