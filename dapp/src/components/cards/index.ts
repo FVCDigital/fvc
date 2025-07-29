246f0b6cb07de28907cde4330ec8228b7c3287c9
Igluminati <IgliKristo@protonmail.com>
2025-07-29 21:44:39 +0100

    Updates card component exports and remove deprecated components

--- dapp/src/components/cards/index.ts
+++ dapp/src/components/cards/index.ts
@@ -2,4 +2,5 @@ export { TradingCard } from './TradingCard';
 export { default as AlertCard } from './AlertCard';
 export { default as BaseCard } from './BaseCard';
 export * from './KYCCard';
-export { default as VotingComingSoonCard } from './VotingComingSoonCard'; 
+export { default as VotingComingSoonCard } from './VotingComingSoonCard';
+export { default as ComingSoonCard } from './ComingSoonCard'; 
