c68cfee6ea3d964b7e5253f8a1a67690215a7027
Igluminati <IgliKristo@protonmail.com>
2025-07-29 21:28:00 +0100

    Implements FVC governance token with vesting restrictions and upgradeable architecture

--- contracts/src/FVC.sol
+++ contracts/src/FVC.sol
@@ -8,12 +8,20 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
 contract FVC is Initializable, ERC20Upgradeable, AccessControlUpgradeable, UUPSUpgradeable {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    
+    // Bonding contract address for vesting checks
+    address public bondingContract;
 
-    function initialize(string memory name, string memory symbol, address admin) public initializer {
-        __ERC20_init(name, symbol);
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize(string memory _name, string memory _symbol, address admin) public initializer {
+        __ERC20_init(_name, _symbol);
         __AccessControl_init();
         __UUPSUpgradeable_init();
-
+        
         _grantRole(DEFAULT_ADMIN_ROLE, admin);
         _grantRole(MINTER_ROLE, admin);
     }
@@ -22,5 +30,34 @@ contract FVC is Initializable, ERC20Upgradeable, AccessControlUpgradeable, UUPSU
         _mint(to, amount);
     }
 
-    function _authorizeUpgrade(address) internal override onlyRole(DEFAULT_ADMIN_ROLE) {}
+    function setBondingContract(address _bondingContract) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        bondingContract = _bondingContract;
+        emit BondingContractSet(_bondingContract);
+    }
+
+    function transfer(address to, uint256 amount) public virtual override returns (bool) {
+        _checkVesting(msg.sender);
+        return super.transfer(to, amount);
+    }
+
+    function transferFrom(address from, address to, uint256 amount) public virtual override returns (bool) {
+        _checkVesting(from);
+        return super.transferFrom(from, to, amount);
+    }
+
+    function _checkVesting(address from) internal view {
+        if (bondingContract != address(0)) {
+            (bool success, bytes memory data) = bondingContract.staticcall(
+                abi.encodeWithSignature("isLocked(address)", from)
+            );
+            if (success && abi.decode(data, (bool))) {
+                revert("FVC: tokens locked in vesting");
+            }
+        }
+    }
+
+    function _authorizeUpgrade(address newImplementation) internal override onlyRole(DEFAULT_ADMIN_ROLE) {}
+
+    // FVC-specific events
+    event BondingContractSet(address indexed bondingContract);
 } 
