e07e0c535d6c8428929d808a2c823c757780584f
Igluminati <IgliKristo@protonmail.com>
2025-07-29 21:48:23 +0100

    Updates screen exports and remove deprecated screen components

--- dapp/src/screens/index.ts
+++ dapp/src/screens/index.ts
@@ -1,5 +1,4 @@
-export { default as OnboardingScreen } from "./onboarding/onboardingScreen";
-export { default as HomeScreen } from "./home/index";
+export { default as HomeScreen } from "./home/HomeScreen";
 // Add more exports here as you add more screens, e.g.:
 // export { default as BondingScreen } from "./bonding/bondingScreen";
 // export { default as StakingScreen } from "./staking/stakingScreen"; 
