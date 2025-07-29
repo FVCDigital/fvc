8bc27f0ec92a73de5f2ec6f9e2badba3816ab4f5
Igluminati <IgliKristo@protonmail.com>
2025-07-29 21:24:07 +0100

    Removes unsupported colors property from RainbowKit theme

--- dapp/src/pages/_app.tsx
+++ dapp/src/pages/_app.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import '@/styles/globals.css';
 import '@rainbow-me/rainbowkit/styles.css';
 import type { AppProps } from 'next/app';
-import { getDefaultConfig, RainbowKitProvider, lightTheme } from '@rainbow-me/rainbowkit';
+import { getDefaultConfig, RainbowKitProvider, darkTheme } from '@rainbow-me/rainbowkit';
 import { WagmiProvider } from 'wagmi';
 import { mainnet, polygon, polygonAmoy } from 'wagmi/chains';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
@@ -24,11 +24,12 @@ export default function App({ Component, pageProps }: AppProps) {
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
         <RainbowKitProvider
-          theme={lightTheme({
+          theme={darkTheme({
             accentColor: theme.generalButton,
             accentColorForeground: theme.buttonText,
             borderRadius: 'medium',
-            fontStack: 'system'
+            fontStack: 'system',
+            overlayBlur: 'small',
           })}
         >
           <Component {...pageProps} />
