6ed70ad7cb7b0a4950e27156f405ccbdbb522485
Igluminati <IgliKristo@protonmail.com>
2025-08-26 09:32:25 +0100

    fixes build error by removing broken FVCProtocolFeatures import

--- dapp/src/components/cards/index.ts
+++ dapp/src/components/cards/index.ts
@@ -6,4 +6,4 @@ export { default as VotingComingSoonCard } from './VotingComingSoonCard';
 export { default as ComingSoonCard } from './ComingSoonCard';
 export { default as PrivateSaleCard } from './PrivateSaleCard';
 export { default as FVCFeatureCard } from './FVCFeatureCard';
-export { default as FVCProtocolFeatures } from './FVCProtocolFeatures'; 
+ 
