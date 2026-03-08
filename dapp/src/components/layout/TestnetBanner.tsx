77fae7d942bbf6ccfbee3d965c6b3bf081e13af8
Igli Kristo <IgliKristo@protonmail.com>
2026-03-08 15:03:32 +0100

    removes testnet banner by returning null from component

--- dapp/src/components/layout/TestnetBanner.tsx
+++ dapp/src/components/layout/TestnetBanner.tsx
@@ -1,26 +1,7 @@
 import React from 'react';
 
 const TestnetBanner: React.FC = () => {
-  return (
-    <div style={{
-      background: 'linear-gradient(90deg, #FCD34D 0%, #FBBF24 100%)',
-      color: '#000000',
-      padding: '8px 16px',
-      textAlign: 'center',
-      fontSize: 14,
-      fontWeight: 600,
-      fontFamily: 'Inter, sans-serif',
-      borderBottom: '1px solid #F59E0B',
-      position: 'fixed',
-      top: 64,
-      left: 280,
-      right: 0,
-      zIndex: 1000,
-      boxSizing: 'border-box',
-    }}>
-      Get testnet FVC
-    </div>
-  );
+  return null;
 };
 
 export default TestnetBanner;
